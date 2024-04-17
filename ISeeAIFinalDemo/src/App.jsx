7387a5ccee373d54a4db7e455db4c5a7129d3d4d
Aniket Rindhe <aniket.m.rindhe@gmail.com>
2024-04-17 10:10:52 +0530

    News Function updated

--- ISeeAIFinalDemo/src/App.jsx
+++ ISeeAIFinalDemo/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Body from './components/Body'
 
-
 function App() {
 
   return (
--- ISeeAIFinalDemo/src/customHooks/NewsUpdate.jsx
+++ ISeeAIFinalDemo/src/customHooks/NewsUpdate.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { TexttoVoice } from './TexttoVoice';
-import { useState } from 'react';
 
 export function NewsUpdate(transcript, setTranscript) {
+    let fetchDataCalled = false;
 
-        let count=0;
-
-        const fetchData = async () => {
-            const res = await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=15c67e5f0742490ab70606684d6768e0");
-            let data = await res.json();
-            TexttoVoice("Todays Top 10 are");
-            setTranscript("Todays Top 10 News");
-            for (let i = 0; i < 10; i++) {
-                TexttoVoice(data.articles[i].title);
-                TexttoVoice(data.articles[i].description);
-                console.log(data.articles[i].title);
+    const fetchData = async () => {
+        if (!fetchDataCalled) {
+            fetchDataCalled = true;
+            try {
+                const res = await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=15c67e5f0742490ab70606684d6768e0");
+                const data = await res.json();
+                TexttoVoice("Todays Top 10 are");
+                setTranscript("Todays Top 10 News");
+                for (let i = 0; i < 10; i++) {
+                    TexttoVoice(data.articles[i].title);
+                    TexttoVoice(data.articles[i].description);
+                    console.log(data.articles[i].title);
+                }
+            } catch (error) {
+                console.error('Error fetching news:', error);
             }
-        };
+        }
+    };
 
-        fetchData();
-    
-    return null; 
+    fetchData();
+
+    return null;
 }
