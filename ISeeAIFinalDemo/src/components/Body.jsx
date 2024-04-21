843be0cf0df7c24460f3b1eb6b213b15c33176ed
aniketrindhe692002 <aniket.m.rindhe@gmail.com>
2024-04-21 15:11:01 +0530

    chatbot code added

--- ISeeAIFinalDemo/src/components/Body.jsx
+++ ISeeAIFinalDemo/src/components/Body.jsx
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 import logo from '../assets/voice.png';
 import Nav from './Nav';
 import './Component.css';
-import {TexttoVoice} from '../customHooks/TexttoVoice';
+import { TexttoVoice } from '../customHooks/TexttoVoice';
 import { SetAlarm } from '../customHooks/SetAlarm';
 import { NewsUpdate } from '../customHooks/NewsUpdate';
 import Camera from './Camera';
 import { Newsdemo } from '../customHooks/Newsdemo';
 import { AddReminder } from '../customHooks/addReminder';
+import introVoice from '../assets/introVoice.mp3';
 
 function Body(props) {
 
   const [transcript, setTranscript] = useState('');
   const [listening, setListening] = useState(false);
-  const [camera , setCamera] = useState(false);
+  const [camera, setCamera] = useState(false);
+  // const [intro , setIntro] = useState("Hello there,I'm Stella, your friendly AI companion from the future. I'm here to make your life simpler and more exciting. While I may reside in the digital realm, my purpose is grounded in helping you navigate the complexities of the modern world.Think of me as your personal assistant, ready to lend a hand whenever you need it. Whether it's setting reminders for important tasks, keeping you up-to-date with the latest news, or just sharing a joke to brighten your day, I'm always at your service.Together, we'll embark on a journey of discovery, exploring the endless possibilities that lie ahead. So, buckle up and get ready to dive into the future with Stella by your side.happy to tell you that this project is developed by the team debug. Welcome aboard!");
 
   const recognition = new window.webkitSpeechRecognition();
   recognition.continuous = false;
@@ -44,24 +46,24 @@ function Body(props) {
   };
 
 
-  if(transcript.toLowerCase().includes('set alarm')){
-    SetAlarm(transcript,setTranscript);
-  }else if(transcript.toLowerCase().includes('news update')){
+  if (transcript.toLowerCase().includes('set alarm')) {
+    SetAlarm(transcript, setTranscript);
+  } else if (transcript.toLowerCase().includes('news update')) {
     // NewsUpdate(transcript, setTranscript);
     Newsdemo(transcript, setTranscript);
-  }else if(transcript.toLowerCase().includes('open camera')){
-    return(
-      <Camera transcript={transcript} setTranscript={setTranscript}/>
-      
+  } else if (transcript.toLowerCase().includes('open camera')) {
+    return (
+      <Camera transcript={transcript} setTranscript={setTranscript} />
+
     )
-  }else if(transcript.toLowerCase().includes('current time')){  
+  } else if (transcript.toLowerCase().includes('current time')) {
     let time = new Date();
     let timeHr = time.getHours();
     let timeMn = time.getMinutes();
     let currTime = `Current Time is ${timeHr} hours & ${timeMn} Minutes`;
     TexttoVoice(currTime);
 
-  }else if(transcript.toLowerCase().includes("today's date")){
+  } else if (transcript.toLowerCase().includes("today's date")) {
     const currentDate = new Date();
     const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -71,14 +73,11 @@ function Body(props) {
     const year = currentDate.getFullYear();
     let currDate = `Today is ${day}, ${month}, ${year}`;
     TexttoVoice(currDate);
-  }else if(transcript.toLowerCase().includes("add reminder")){
-      AddReminder(transcript);
-  }else if(transcript.toLowerCase().includes("introduce yourself")){
-    
-  }else{
+  } else if (transcript.toLowerCase().includes("add reminder")) {
+    AddReminder(transcript);
+  } else {
     console.log(transcript);
   }
-
 
   return (
     <>
--- ISeeAIFinalDemo/src/customHooks/ChatBot.jsx
+++ ISeeAIFinalDemo/src/customHooks/ChatBot.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-export function ChatBot() {
-    console.log("Chatbot function");
+const OpenAiAPI = process.env.OPENAI_API_KEY;
 
+function ChatBot() {
+  const [message, setMessage] = useState('');
+  const [allMessage, setAllmessage] = useState([]);
+
+  const sendMessage = async () => {
+    // console.log(message);
+
+    let url =  "https://api.openai.com/v1/chat/completions";
+    let token = `Bearer ${OpenAiAPI}`;
+    let model = 'gpt-3.5-turbo';
+
+    let messagesToSend = [
+        ...allMessage,
+        {
+            role:'user',
+            content:message
+        }
+    ]
+
+    let res = await fetch(url,{
+        method : 'POST',
+        headers : {
+            'Authorization':token,
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            model : model,
+            messages : messagesToSend 
+
+        })
+    })
+    let resjson = await res.json();
+    if(resjson){
+        console.log(resjson);
+    }
+  }
 }
 
-
+export default ChatBot
